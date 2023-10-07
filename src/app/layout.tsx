53d7de507d75994a6514e300eb2f4f701cc8676f
Tom Wilkinson <twilkinson3421@outlook.com>
2023-10-07 23:09:02 +0100

    Added redirects for protected routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.scss';
 import type { Metadata } from 'next';
+import { AuthProvider } from './providers';
 
 export const metadata: Metadata = {
   title: 'BTK Spiel',
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
--- src/app/zugang/layout.tsx
+++ src/app/zugang/layout.tsx
@@ -3,11 +3,11 @@ import { getServerSession } from 'next-auth';
 import { config } from '@/helpers/auth';
 import { redirect } from 'next/navigation';
 
-export default async function FeaturesLayout({ children }: { children: React.ReactNode }) {
+export default async function ZugangLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(config);
 
-  if (!session) {
-    redirect('/zugang/login');
+  if (session) {
+    redirect('/features/account');
   }
 
   return <Fragment>{children}</Fragment>;
