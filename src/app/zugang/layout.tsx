2df629e6c1d92192dba04c894e9896cf6a0a809b
Tom Wilkinson <twilkinson3421@outlook.com>
2023-10-11 16:54:20 +0100

    Added metadata for zugang pages

--- src/app/zugang/layout.tsx
+++ src/app/zugang/layout.tsx
@@ -2,6 +2,12 @@ import { Fragment } from 'react';
 import { getServerSession } from 'next-auth';
 import { config } from '@/helpers/auth';
 import { redirect } from 'next/navigation';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Zugang | BTK Spiel',
+  description: 'Zugang | BTK Spiel',
+};
 
 export default async function ZugangLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(config);
