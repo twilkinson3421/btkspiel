1772a58975b980ed04e2374cafbb23e8795e46b1
Tom Wilkinson <twilkinson3421@outlook.com>
2023-10-13 21:13:48 +0100

    Escaped quote characters on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PageWrapper from '@/components/PageWrapper/PageWrapper';
 export default async function Home() {
   return (
     <PageWrapper>
-      <h1>This page is no longer available, please use "/games" instead and report this issue</h1>
+      <h1>This page is no longer available, please use &quot;/games&quot; instead and report this issue</h1>
     </PageWrapper>
   );
 }
